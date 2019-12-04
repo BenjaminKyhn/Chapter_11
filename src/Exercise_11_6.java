0a8938c39cb2a4f2146c7ba3fc4c7f1d7e1980b9
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-12-04 09:42:39 +0100

    Exercise 11.6 added

--- src/Exercise_11_6.java
+++ src/Exercise_11_6.java
@@ -1,8 +1,16 @@
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Exercise_11_6 {
     public static void main(String[] args) {
         ArrayList list = new ArrayList();
-        list.add()
+        list.add(new Date());
+        list.add(new Circle());
+        list.add(new Loan());
+        list.add(new String("String class"));
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).toString());
+        }
     }
 }
