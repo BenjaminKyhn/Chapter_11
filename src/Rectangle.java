df45e0ac9c404a58e04ee6cac1d01639b6d6125d
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-12-04 10:05:25 +0100

    Listing 11.3 added

---
+++ src/Rectangle.java
@@ -0,0 +1,42 @@
+import org.w3c.dom.css.Rect;
+
+public class Rectangle extends GeometricObject {
+    private double height;
+    private double width;
+
+    public Rectangle(){
+    }
+
+    public Rectangle(double width, double height, String colour, boolean filled){
+        this.width = width;
+        this.height = height;
+        setColour(colour);
+        setFilled(filled);
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    // Return area
+    public double getArea(){
+        return width * height;
+    }
+
+    // Return perimeter
+    public double getPerimeter(){
+        return 2 * (width + height);
+    }
+}
